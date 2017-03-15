a31ecfef01a0b52bfe3ab005dc0a1ac1a1854590
Dave Stewart <dave@davestewart.co.uk>
2017-03-15 20:05:43 +0000

    Updated to scroll selections of objects, allowing passing of bounds, plus minor improvements

--- jquery.scroll-into-view.js
+++ jquery.scroll-into-view.js
@@ -7,56 +7,97 @@
  * @author Dave Stewart
  * @date 14 March 2017
  */
-(function ($, window, document, undefined) {
-  'use strict'
-
+(function ($) {
   /**
    * @property  {Number|String|HTMLElement}   vt          Viewport top; can be a number, selector or element
    * @property  {Number|String|HTMLElement}   vb          Viewport bottom; can be a number, selector or element
    * @property  {Number|String}               pt          Padding top; can be a number, string percent value, or the string 'auto'
    * @property  {Number}                      pb          Padding bottom
-   * @property  {Number}                      duration    Scroll duration; in milliseconds
+   * @property  {Object|Function}             bounds      Custom bounds; pass in a bounds object {top, bottom, height} or a function that returns the same: getBounds(elements)
+   * @property  {Function}                    fn          Optional scroll function to call instead of the default; scrollTo(value, options, values)
    * @property  {Boolean}                     force       Force the element to scroll into position, even if it's already on-screen
-   * @property  {Function}                    fn          Optional function to call instead of the default. Takes a pixel value and duration as its 2 arguments
+   * @property  {Number}                      duration    Scroll duration; in milliseconds
    */
   var defaults = {
     vt: 0,
     vb: 0,
     pt: 20,
     pb: 20,
-    duration: 400,
-    fn: null
+    bounds: null,
+    fn: null,
+    force: false,
+    duration: 400
   }
 
-  // constants
-  var $parent = $('html, body')
-  var $body = $('body')
-
   // helper functions
+  function getAllElements (elements) {
+    return $(elements)
+      .map(function (i, e) {
+        return $(e).get(0)
+      })
+      .toArray()
+  }
+
   function getHeight (value) {
     return typeof value === 'number'
       ? value
       : $(value).height()
   }
 
+  function getElementBounds (element) {
+    var b = element.getBoundingClientRect()
+    return {
+      top: b.top,
+      bottom: b.bottom,
+      height: b.height
+    }
+  }
+
+  function getSelectionBounds (elements) {
+    var bounds = null
+    getAllElements(elements)
+      .forEach(function (element) {
+        var b = getElementBounds(element)
+        if (!bounds) {
+          bounds = b
+        }
+        else {
+          if (b.top < bounds.top) {
+            bounds.top = b.top
+          }
+          if (b.bottom > bounds.bottom) {
+            bounds.bottom = b.bottom
+          }
+        }
+      })
+    bounds.height = bounds.bottom - bounds.top
+    return bounds
+  }
+
   function scrollTo (value, options) {
-    $parent.animate({scrollTop: value, duration: options.duration})
+    $('html, body').animate({scrollTop: value}, options.duration)
   }
 
   // main function
-  function scroll (element, opts) {
+  function scroll (elements, opts) {
     // options
     var options = $.extend({}, defaults, opts)
-    var bounds = element.getBoundingClientRect()
 
     // window
     var wh = getHeight(window)
-    var wy = $body.scrollTop()
+    var wy = document.body.scrollTop
 
     // viewport
     var vt = getHeight(options.vt)
     var vb = getHeight(options.vb)
     var vh = wh - vt - vb
+
+    // bounds
+    var bounds = options.bounds
+      ? typeof options.bounds === 'function'
+        ? options.bounds(elements)
+        : options.bounds
+      : getSelectionBounds(elements)
 
     // padding
     var pt = options.pt
@@ -73,7 +114,7 @@
       pb = Math.floor(pd * (1 - pc))
     }
 
-    // element (absolute)
+    // element bounds
     var eh = bounds.height + pt + pb
     var et = bounds.top - pt
     var eb = bounds.bottom + pb
@@ -84,7 +125,7 @@
 
     // final values
     var fn = options.fn || scrollTo
-    var values = {wh, wy, vt, vb, vh, pt, pb, eh, et, eb, ot, ob}
+    var values = {wh: wh, wy: wy, vt: vt, vb: vb, vh: vh, pt: pt, pb: pb, eh: eh, et: et, eb: eb, ot: ot, ob: ob}
 
     // scroll
     if (ot <= 0 || (ob >= 0 && eh > vh) || options.force) {
@@ -97,12 +138,12 @@
 
   // jQuery plugin function
   var scrollIntoView = function (options) {
-    scroll(this.get(0), options)
+    scroll(this.toArray(), options)
     return this
   }
 
-  // convenience method to set defaults
-  scrollIntoView.defaults = function (options) {
+  // set / get default options
+  scrollIntoView.options = function (options) {
     if (options) {
       $.extend(defaults, options)
     }
@@ -111,4 +152,4 @@
 
   // assign plugin
   $.fn.scrollIntoView = scrollIntoView
-})(jQuery, window, document)
+})(jQuery)
